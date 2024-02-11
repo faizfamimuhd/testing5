b8a635d6701217fa136ca6a338f77f178fa17e4d
faiz fami <faizfami99@gmail.com>
2024-02-11 21:07:44 +0800

    Add project files.

---
+++ testing5/Scripts/response.js
@@ -0,0 +1,6 @@
+const responseObj = {
+  "1": "Sila layari https://ebiz.jupem.gov.my/Permohonan/PermohonanUtama",
+  "2": "Proses naik taraf pengguna hanya boleh dilakukan di kaunter JUPEM di setiap negeri. Pihak kaunter akan membuat pengesahan kad pengenalan pelanggan menggunakan pengimbas cap jari.Apabila semakan kad pengenalan telah disahkan adalah milik pelanggan tersebut, akaun pelanggan akan dinaik taraf kepada Pelanggan Utama.",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+};
